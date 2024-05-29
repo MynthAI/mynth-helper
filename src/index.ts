5513c84d0baae2e27e5d10e12b7a6e6a64847781
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2024-05-29 11:40:53 +0100

    Removed mynth-logger dependency

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,3 @@ export * from "./helpers/cardano/index.js";
 export * from "./helpers/common/index.js";
 export * from "./helpers/filesystem/index.js";
 export * from "./helpers/lucid/index.js";
-export { setupLogging } from "mynth-logger";
